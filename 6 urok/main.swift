6a56771d043376ae55f326db1a2269db9fca885b
keikod1 <madragimova@icloud.com>
2021-11-17 20:28:57 +0600

    Initial Commit

---
+++ 6 urok/main.swift
@@ -0,0 +1,33 @@
+//
+//  main.swift
+//  6 urok
+//
+//  Created by my macbook on 17/11/21.
+//
+
+import Foundation
+
+
+//var name: String? = "Nursultan"
+//print(name)
+//
+//var name2: String = "Nursultan"
+//print(name2)
+
+//func sum (){
+//    let sum = (Int(number1!) ?? 0) + (Int(number2!) ?? 0)
+//}
+// sum()
+//
+//if let number1 = number1{
+//    print(number1)
+//}else{
+//    print(number1)
+//}
+
+print("введите имя пользователя:")
+var number1 = readLine()
+
+print("введите пароль:")
+var number2 = readLine()
+
